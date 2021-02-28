d39453efeadbff8e93ae7d12a98f3bb3dc5ad1d8
opFez <opfez@disroot.org>
2021-02-28 22:42:54 +0100

    tabs -> spaces

--- main.scm
+++ main.scm
@@ -1,13 +1,13 @@
 (import (r7rs)
-		(srfi-1)
-		(srfi-13)
-		(srfi-193)
-		(ncurses)
-		(shell)
-		(chicken io)
-		(chicken file posix)
-		;; while, should switch to simple let loop
-		miscmacros)
+        (srfi-1)
+        (srfi-13)
+        (srfi-193)
+        (ncurses)
+        (shell)
+        (chicken io)
+        (chicken file posix)
+        ;; while, should switch to simple let loop
+        miscmacros)
 
 (load "utility.scm")
 
@@ -15,9 +15,9 @@
 
 (define +args+ (cdr (command-line)))
 (if (= 0 (length +args+))
-	(begin (display "usage: te filename")
-		   (newline)
-		   (exit)))
+    (begin (display "usage: te filename")
+           (newline)
+           (exit)))
 
 ;; Global file-buffer
 
@@ -33,43 +33,43 @@
 (define (draw-empty-part line-number max-rows)
   (mvprintw line-number 0 "~")
   (if (= line-number max-rows)
-	  #f
-	  (draw-empty-part (add1 line-number) max-rows)))
+      #f
+      (draw-empty-part (add1 line-number) max-rows)))
 
 (define (draw-file-buffer row fb)
   (mvprintw row 0 (car fb))
   (if (= 0 (length (cdr fb)))
-	  #f
-	  (draw-file-buffer (add1 row) (cdr fb))))
+      #f
+      (draw-file-buffer (add1 row) (cdr fb))))
 
 
 ;; Movement
 
 (define (move-up buffer coords)
   (cons (get-x coords)
-		(safe-sub1 (get-y coords))))
+        (safe-sub1 (get-y coords))))
 
 (define (move-down buffer coords +max-vals+)
   (cons (get-x coords)
-		(safe-add1 (get-y coords) (sub1 (length file-buffer)))))
+        (safe-add1 (get-y coords) (sub1 (length file-buffer)))))
 
 (define (move-left buffer coords)
   (cons (safe-sub1 (get-x coords))
-		(get-y coords)))
+        (get-y coords)))
 
 (define (move-right buffer coords +max-vals+)
   (let ((line-length (length (string->list (list-ref buffer (get-y coords))))))
-	(if (< (get-x coords) line-length)
-		(cons (safe-add1 (get-x coords) (get-x +max-vals+))
-			  (get-y coords))
-		(cons line-length (get-y coords)))))
+    (if (< (get-x coords) line-length)
+        (cons (safe-add1 (get-x coords) (get-x +max-vals+))
+              (get-y coords))
+        (cons line-length (get-y coords)))))
 
 (define (move-bol coords)
   (cons 0 (get-y coords)))
 
 (define (move-eol buffer coords)
   (cons (length (string->list (list-ref buffer (get-y coords))))
-		(get-y coords)))
+        (get-y coords)))
 
 
 ;; Editing
@@ -78,31 +78,31 @@
 
 (define (delete-char buffer coords)
   (let ((head (take (string->list (list-ref buffer (get-y coords))) (get-x coords)))
-		(tail (drop (string->list (list-ref buffer (get-y coords))) (get-x coords))))
-	(if (= 0 (length (string->list (list-ref buffer (get-y coords)))))
-		(append (take buffer (get-y coords))
-				(drop buffer (add1 (get-y coords))))
-		(append (take buffer (get-y coords))
-				(if (= 0 (length tail))
-					(list (list->string (reverse (cdr (reverse head)))))
-					(list (list->string (append (reverse (cdr (reverse head))) tail))))
-				(drop buffer (add1 (get-y coords)))))))
+        (tail (drop (string->list (list-ref buffer (get-y coords))) (get-x coords))))
+    (if (= 0 (length (string->list (list-ref buffer (get-y coords)))))
+        (append (take buffer (get-y coords))
+                (drop buffer (add1 (get-y coords))))
+        (append (take buffer (get-y coords))
+                (if (= 0 (length tail))
+                    (list (list->string (reverse (cdr (reverse head)))))
+                    (list (list->string (append (reverse (cdr (reverse head))) tail))))
+                (drop buffer (add1 (get-y coords)))))))
 
 (define (input-char buffer coords c)
   (append (take buffer (get-y coords))
-		  (list (string-insert (list-ref buffer (get-y coords)) (get-x coords) (string c)))
-		  (drop buffer (+ (get-y coords) 1))))
+          (list (string-insert (list-ref buffer (get-y coords)) (get-x coords) (string c)))
+          (drop buffer (+ (get-y coords) 1))))
 
 
 ;; I/O
 (define (save-file buffer filename)
   (let ((outf (file-open filename (+ open/write open/wronly))))
-	(let loop ((lines-to-write buffer))
-	  (if (= 0 (length lines-to-write))
-		  (file-close outf)
-		  (begin (file-write outf (car lines-to-write))
-				 (file-write outf "\n")
-				 (loop (cdr lines-to-write)))))))
+    (let loop ((lines-to-write buffer))
+      (if (= 0 (length lines-to-write))
+          (file-close outf)
+          (begin (file-write outf (car lines-to-write))
+                 (file-write outf "\n")
+                 (loop (cdr lines-to-write)))))))
 
 ;; Trigger this function on exiting the program
 (on-exit endwin)
@@ -114,59 +114,59 @@
   (cbreak)
   (noecho)
   (let ((should-exit #f)
-		(user-coords (cons 0 0)) ; x y
-		(last-key-escape? #f))
-	(while (not should-exit)
-	  (clear)
-	  (curs_set 0) ;; hide cursor for drawing
-	  (draw-file-buffer 0 file-buffer)
-	  (draw-empty-part (length file-buffer) (get-y +max-vals+))
+        (user-coords (cons 0 0)) ; x y
+        (last-key-escape? #f))
+    (while (not should-exit)
+      (clear)
+      (curs_set 0) ;; hide cursor for drawing
+      (draw-file-buffer 0 file-buffer)
+      (draw-empty-part (length file-buffer) (get-y +max-vals+))
 
-	  (move (get-y user-coords) (get-x user-coords))
-	  (curs_set 1) ;; show cursor again
+      (move (get-y user-coords) (get-x user-coords))
+      (curs_set 1) ;; show cursor again
 
-	  (refresh)
+      (refresh)
 
-	  (let ((c (getch)))
-		(if last-key-escape?
-			(begin
-			  (set! last-key-escape? #f)
-			  (cond ((eq? c #\j) (set! user-coords
-								   (move-left file-buffer user-coords)))
-					((eq? c #\k) (set! user-coords
-								   (move-down file-buffer user-coords +max-vals+)))
-					((eq? c #\l) (set! user-coords
-								   (move-up file-buffer user-coords)))
-					((eq? c #\;) (set! user-coords
-								   (move-right file-buffer user-coords +max-vals+)))
-					((eq? c #\a) (set! user-coords
-								   (move-bol user-coords)))
-					((eq? c #\f) (set! user-coords
-								   (move-eol file-buffer user-coords)))
-					((eq? c #\q) (set! should-exit #t))
-					((eq? c #\d) (begin
-								   (set! user-coords
-									 (move-right file-buffer user-coords +max-vals+))
-								   (set! file-buffer (delete-char file-buffer user-coords))
-								   (set! user-coords (cons (safe-sub1 (get-x user-coords))
-														   (get-y user-coords)))))
-					((eq? c #\s) (begin (set! *buffer-changed* #f)
-										(save-file file-buffer (car +args+))))))
-			;; else if
-			(cond ((eq? c #\esc)
-				   (set! last-key-escape? #t))
-				  ;; not a shortcut, input literal character
-				  (#t (begin (set! *buffer-changed* #t)
-							 (set! file-buffer (input-char file-buffer user-coords c))
-							 (set! user-coords (cons (safe-add1 (get-x user-coords)
-																(get-x +max-vals+))
-													 (get-y user-coords))))))))
-	  
+      (let ((c (getch)))
+        (if last-key-escape?
+            (begin
+              (set! last-key-escape? #f)
+              (cond ((eq? c #\j) (set! user-coords
+                                   (move-left file-buffer user-coords)))
+                    ((eq? c #\k) (set! user-coords
+                                   (move-down file-buffer user-coords +max-vals+)))
+                    ((eq? c #\l) (set! user-coords
+                                   (move-up file-buffer user-coords)))
+                    ((eq? c #\;) (set! user-coords
+                                   (move-right file-buffer user-coords +max-vals+)))
+                    ((eq? c #\a) (set! user-coords
+                                   (move-bol user-coords)))
+                    ((eq? c #\f) (set! user-coords
+                                   (move-eol file-buffer user-coords)))
+                    ((eq? c #\q) (set! should-exit #t))
+                    ((eq? c #\d) (begin
+                                   (set! user-coords
+                                     (move-right file-buffer user-coords +max-vals+))
+                                   (set! file-buffer (delete-char file-buffer user-coords))
+                                   (set! user-coords (cons (safe-sub1 (get-x user-coords))
+                                                           (get-y user-coords)))))
+                    ((eq? c #\s) (begin (set! *buffer-changed* #f)
+                                        (save-file file-buffer (car +args+))))))
+            ;; else if
+            (cond ((eq? c #\esc)
+                   (set! last-key-escape? #t))
+                  ;; not a shortcut, input literal character
+                  (#t (begin (set! *buffer-changed* #t)
+                             (set! file-buffer (input-char file-buffer user-coords c))
+                             (set! user-coords (cons (safe-add1 (get-x user-coords)
+                                                                (get-x +max-vals+))
+                                                     (get-y user-coords))))))))
+      
 
-	  ;; Exploit the fact that moving right correctly places you on the end of the line
-	  ;; for compensating moving up or down outside the range of a line.
-	  (when (> (get-x user-coords)
-			   (length (string->list (list-ref file-buffer (get-y user-coords)))))
-		(set! user-coords (move-right file-buffer user-coords +max-vals+))))))
+      ;; Exploit the fact that moving right correctly places you on the end of the line
+      ;; for compensating moving up or down outside the range of a line.
+      (when (> (get-x user-coords)
+               (length (string->list (list-ref file-buffer (get-y user-coords)))))
+        (set! user-coords (move-right file-buffer user-coords +max-vals+))))))
 
 (main)
--- utility.scm
+++ utility.scm
@@ -2,8 +2,8 @@
 ;; Utility functons used by other functions.
 
 (import (r7rs)
-		(shell)
-		(srfi-13))
+        (shell)
+        (srfi-13))
 
 (define (string-insert s i t)
   (string-replace s t i i))
@@ -23,14 +23,14 @@
 (: safe-sub1 (number -> number))
 (define (safe-sub1 n)
   (if (= n 0)
-	  0
-	  (sub1 n)))
+      0
+      (sub1 n)))
 
 (: safe-add1 (number number -> number))
 (define (safe-add1 n max-val)
   (if (= n max-val)
-	  n
-	  (add1 n)))
+      n
+      (add1 n)))
 
 (define (get-max-columns)
   (sub1 (string->number (remove-trailing-newline (capture (tput cols))))))
@@ -41,13 +41,13 @@
 ;; modified https://stackoverflow.com/a/63548144
 (define (read-n-lines file n)
   (let ((infile (open-input-file file)))
-	(let loop ((lines '())
-			   (next-line (read-line infile))
-			   (lines-read 0))
-	  (if (or (eof-object? next-line)
-			  (= lines-read n))
-		  (begin (close-input-port infile)
-				 (reverse lines))
-		  (loop (cons next-line lines)
-				(read-line infile)
-				(add1 lines-read))))))
+    (let loop ((lines '())
+               (next-line (read-line infile))
+               (lines-read 0))
+      (if (or (eof-object? next-line)
+              (= lines-read n))
+          (begin (close-input-port infile)
+                 (reverse lines))
+          (loop (cons next-line lines)
+                (read-line infile)
+                (add1 lines-read))))))
